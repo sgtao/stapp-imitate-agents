a01422ede3ca9d9a6c212f497904f9ff5f9a2554
Shogo Ogami <sg.tao.so@gmail.com>
2025-09-02 19:54:26 +0900

    initial commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+"""
+# Welcome to Streamlit App. to imitate agents
+
+"""
+
+# サイドバーのページに移動
+# st.page_link("pages/example_app.py", label="Go to Example App")
+st.page_link("pages/01_example_app.py", label="Go to Example App", icon="🚀")
