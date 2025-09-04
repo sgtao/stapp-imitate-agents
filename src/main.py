e5fde90d6ab3fd0e1f421d30f51ba6e899c9b708
Shogo Ogami <sg.tao.so@gmail.com>
2025-09-05 08:14:17 +0900

    feat:update pyproject.toml and src for remove sample code

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import streamlit as st
+# import streamlit as st
 
 """
 # Welcome to Streamlit App. to imitate agents
@@ -7,4 +7,4 @@ import streamlit as st
 
 # サイドバーのページに移動
 # st.page_link("pages/example_app.py", label="Go to Example App")
-st.page_link("pages/01_example_app.py", label="Go to Example App", icon="🚀")
+# st.page_link("pages/01_example_app.py", label="Go to Example App", icon="🚀")
