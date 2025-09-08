26b60744f2b0902031aeb182b42d6964266d6356
Shogo Ogami <sg.tao.so@gmail.com>
2025-09-09 08:05:51 +0900

    refactor:update SideMenus for omit unused ApiKey component

--- src/components/SideMenus.py
+++ src/components/SideMenus.py
@@ -1,7 +1,6 @@
 # SideMenus.py
 import streamlit as st
 
-from components.ApiKey import ApiKey
 from components.ClientController import ClientController
 from components.UserInputs import UserInputs
 
@@ -9,13 +8,11 @@ from components.UserInputs import UserInputs
 class SideMenus:
     def __init__(self):
         # インスタンス化
-        self.api_key_component = ApiKey()
         self.client_controller = ClientController()
         self.user_inputs_component = UserInputs(user_property_path="results")
 
     def render_api_client_menu(self):
         with st.sidebar:
-            self.api_key_component.input_key()
             self.user_inputs_component.render_dynamic_inputs()
             self.user_inputs_component.render_property_path()
             with st.expander("session_state", expanded=False):
