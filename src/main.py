029f5aacaf203b56327a30d6cfe8dc39fc725956
Shogo Ogami <sg.tao.so@gmail.com>
2025-09-18 08:17:12 +0900

    feat:update main page for append a link to chat_with_config page

--- src/main.py
+++ src/main.py
@@ -1,12 +1,20 @@
 import streamlit as st
 
 """
-# Welcome to [Streamlit](https://docs.streamlit.io/) App. to imitate agents
+## Welcome to imitate agents  App.
+- implemented with [Streamlit](https://docs.streamlit.io/).
 
 """
 
 # サイドバーのページに移動
 st.page_link(
-    "pages/11_config_api_client.py", label="Go to API Client App", icon="🧪"
+    "pages/11_config_api_client.py",
+    label="Go to API Client App (for create Config.)",
+    icon="🧪",
+)
+st.page_link(
+    "pages/12_chat_with_config.py",
+    label="Go to Chat with Config App",
+    icon="💬",
 )
 st.page_link("pages/21_logs_viewer.py", label="Go to Log Vieewr", icon="🗒️")
